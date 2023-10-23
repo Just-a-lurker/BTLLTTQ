f5344633fecbba5c5fb86c01560ffceed72f1b20
Just-a-lurker <111871642+Just-a-lurker@users.noreply.github.com>
2023-10-23 13:57:11 +0700

    update1

---
+++ BTLLTTQ/NhapVaBan/FormNhap.cs
@@ -0,0 +1,35 @@
+﻿using BTLLTTQ.Menu;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BTLLTTQ.NhapVaBan
+{
+    public partial class FormNhap : Form
+    {
+        Sql db = new Sql();
+        public FormNhap()
+        {
+            InitializeComponent();
+            DataTable dt = db.DocBang("Select * from hoadonnhap");
+            dataGridView1.DataSource = dt;
+            dataGridView1.Columns[0].HeaderText = "Mã chất liệu";
+            dataGridView1.Columns[1].HeaderText = "Tên chất liệu";
+            dataGridView1.Columns[0].Width = 150;
+            dataGridView1.Columns[1].Width = 250;
+            dataGridView1.BackgroundColor = Color.LightBlue;
+            dt.Dispose();//Giải phóng bộ nhớ cho DataTable
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
