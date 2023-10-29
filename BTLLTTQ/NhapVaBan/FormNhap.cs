97bf04629b290b20c0cf264176b3eee45d0e9af1
Just-a-lurker <111871642+Just-a-lurker@users.noreply.github.com>
2023-10-29 15:01:32 +0700

    Update ban va nhap
    Them Form chitietHDN va hoan thanh form HDN

--- BTLLTTQ/NhapVaBan/FormNhap.cs
+++ BTLLTTQ/NhapVaBan/FormNhap.cs
@@ -37,19 +37,85 @@ namespace BTLLTTQ.NhapVaBan
 
         private void btnThem_Click(object sender, EventArgs e)
         {
-            if (txtHDN.Text == "" || cbbMaNV.Text =="" || cBBmaNCC.Text == "")
+            if (txtHDN.Text == "" || checkMa(txtHDN.Text) || cbbMaNV.Text =="" || cBBmaNCC.Text == "")
             {
-
+                MessageBox.Show("Check lai DL");
             }
             else
             {
-                db.CapNhatDuLieu("insert into hoadonnhap values ("+txtHDN.Text+","+cbbMaNV.Text+",'"+cBBmaNCC.Text+"','"+ dateNgayNhap.Value.Date.ToString("yyyyMMdd")+"',"+int.Parse(txtTongTien.Text) + ")");
+                db.CapNhatDuLieu("insert into hoadonnhap values ("+txtHDN.Text+",N'"+cbbMaNV.Text+"',N'"+cBBmaNCC.Text+"','"+ dateNgayNhap.Value.Date.ToString("yyyyMMdd")+"',"+int.Parse(txtTongTien.Text) + ")");
                 txtHDN.Text = txtTongTien.Text = "";
                 cbbMaNV.SelectedIndex = -1;
                 cBBmaNCC.SelectedIndex = -1;
                 DataTable dt = db.DocBang("Select * from hoadonnhap");
                 dataGridView1.DataSource = dt;
             }
+        }
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+                db.CapNhatDuLieu("delete from hoadonnhap where sohdn =N'" + txtHDN.Text + "'");
+                txtHDN.Text = txtTongTien.Text = "";
+                cbbMaNV.SelectedIndex = -1;
+                cBBmaNCC.SelectedIndex = -1;
+                DataTable dt = db.DocBang("Select * from hoadonnhap");
+                dataGridView1.DataSource = dt;
+        }
+
+        private void btnSua_Click(object sender, EventArgs e)
+        {
+            if (txtHDN.Text == "" || cbbMaNV.Text == "" || cBBmaNCC.Text == "")
+            {
+                MessageBox.Show("Check lai DL");
+            }
+            else
+            {
+                db.CapNhatDuLieu("update hoadonnhap set maNV=N'" + cbbMaNV.Text + "', mancc = N'" + cBBmaNCC.Text + "',ngaynhap ='" + dateNgayNhap.Value.Date.ToString("yyyyMMdd") + "',tongtien =" + int.Parse(txtTongTien.Text) + " where sohdn =N'" + txtHDN.Text + "'");
+                txtHDN.Text = txtTongTien.Text = "";
+                cbbMaNV.SelectedIndex = -1;
+                cBBmaNCC.SelectedIndex = -1;
+                DataTable dt = db.DocBang("Select * from hoadonnhap");
+                dataGridView1.DataSource = dt;
+            }
+        }
+
+        bool checkMa(string k)
+        {
+            DataTable dt = db.DocBang("Select * from hoadonnhap where sohdn =N'" + k + "'");
+            if (dt.Rows.Count > 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private void dataGridView1_Click(object sender, EventArgs e)
+        {
+            txtHDN.Enabled = false;
+            txtHDN.Text = dataGridView1.CurrentRow.Cells[0].Value.ToString();
+            cbbMaNV.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+            cBBmaNCC.Text = dataGridView1.CurrentRow.Cells[2].Value.ToString();
+            dateNgayNhap.Text = dataGridView1.CurrentRow.Cells[3].Value.ToString();
+            txtTongTien.Text = dataGridView1.CurrentRow.Cells[4].Value.ToString();
+        }
+
+        private void FormNhap_Click(object sender, EventArgs e)
+        {
+            txtHDN.Text = txtTongTien.Text = "";
+            cbbMaNV.SelectedIndex = -1;
+            cBBmaNCC.SelectedIndex = -1;
+            txtHDN.Enabled = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (txtHDN.Text == "") MessageBox.Show("Nhap hoac chon ma HDN");
+            else
+            {
+                FormChiTIetHDN chiTIetHDN = new FormChiTIetHDN(txtHDN.Text);
+                chiTIetHDN.ShowDialog();
+            }
+            
         }
     }
 }
