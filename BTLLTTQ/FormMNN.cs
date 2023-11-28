53af23098d9af4b32d9785a3a920af7f16a4f0ca
lee mih duwk <sympathic.elf181203@gmail.com>
2023-11-28 15:00:54 +0700

    s

--- BTLLTTQ/FormMNN.cs
+++ BTLLTTQ/FormMNN.cs
@@ -143,9 +143,9 @@ namespace BTLLTTQ
 
         private void btn_close_Click(object sender, EventArgs e)
         {
-            //Application.Exit();
-            this.Hide();
-            new FormExit().ShowDialog();
+            Application.Exit();
+
+
         }
 
         private void btn_maz_Click(object sender, EventArgs e)
